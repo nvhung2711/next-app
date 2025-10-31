68017234ea9ac1f536ff333046658ca388c28378
nvhung2711 <nvhung2711@gmail.com>
2025-10-31 14:38:27 +1100

    Server component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import ProductCard from '../components/ProductCard';
 
-const UsersPage = () => {
-    return <div>UsersPage</div>;
+interface User {
+    id: number;
+    name: string;
+}
+
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const users: User[] = await res.json();
+
+    return (
+        <div>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+            <ProductCard />
+        </div>
+    );
 };
 
 export default UsersPage;
